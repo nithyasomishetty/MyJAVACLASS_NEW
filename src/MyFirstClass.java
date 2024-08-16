00d8e6db77a392a2bb602d19b6f7507b944f2908
Likinani.1 <Nithyasomishetty@gmail.com>
2024-08-16 11:48:46 +0530

    ading class

---
+++ src/MyFirstClass.java
@@ -0,0 +1,31 @@
+
+public class MyFirstClass {
+	
+	
+	
+	public void myMethod1() {
+		System.out.println("im myMethod1");
+		
+	}
+	
+	public void myMethod2() {
+		System.out.println("im myMethod2");
+	}
+	
+	public void myMethod3() {
+		System.out.println("im method3");
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		MyFirstClass clas=new MyFirstClass();
+		clas.myMethod1();
+		clas.myMethod2();
+		clas.myMethod3();
+		
+	}
+
+}
